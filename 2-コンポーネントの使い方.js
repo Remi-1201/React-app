b1bb036a87dbbc32603c1736af3ffb40e5ea2c2e
Remi <97021497+Remi-1201@users.noreply.github.com>
2022-06-10 17:00:54 +0900

    イベントやstyleの扱い方.js

--- 2-コンポーネントの使い方.js
+++ 2-コンポーネントの使い方.js
@@ -1,26 +1,5 @@
-// App.js ====================
-import React from "react";
-
-const App = () => {
-  return (
-    <>
-      <h1>Hello</h1>
-      <p>How are you?</p>
-    </>
-  );
-};
-
-// to make App function can be used in other files
-export default App;
-
-// index.js ====================
-import React from "react";
-import ReactDom from "react-dom";
-
-// component was imported from App.js
-import App from "./App";
-
-// App関数 は App.js  でコンポネント化されたため削除可能
+// // App.js ====================
+// import React from "react";
 
 // const App = () => {
 //   return (
@@ -31,4 +10,25 @@ import App from "./App";
 //   );
 // };
 
-ReactDom.render(<App />, document.getElementById("root"));
+// // to make App function can be used in other files
+// export default App;
+
+// // index.js ====================
+// import React from "react";
+// import ReactDom from "react-dom";
+
+// // component was imported from App.js
+// import App from "./App";
+
+// // App関数 は App.js  でコンポネント化されたため削除可能
+
+// // const App = () => {
+// //   return (
+// //     <>
+// //       <h1>Hello</h1>
+// //       <p>How are you?</p>
+// //     </>
+// //   );
+// // };
+
+// ReactDom.render(<App />, document.getElementById("root"));
