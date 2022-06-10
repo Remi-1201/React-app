34af0aa8d8459d880f19798f6ff6382854602788
Remi <97021497+Remi-1201@users.noreply.github.com>
2022-06-10 16:35:22 +0900

    コンポーネントの使い方

---
+++ 2-コンポーネントの使い方.js
@@ -0,0 +1,34 @@
+// App.js ====================
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <h1>Hello</h1>
+      <p>How are you?</p>
+    </>
+  );
+};
+
+// to make App function can be used in other files
+export default App;
+
+// index.js ====================
+import React from "react";
+import ReactDom from "react-dom";
+
+// component was imported from App.js
+import App from "./App";
+
+// App関数 は App.js  でコンポネント化されたため削除可能
+
+// const App = () => {
+//   return (
+//     <>
+//       <h1>Hello</h1>
+//       <p>How are you?</p>
+//     </>
+//   );
+// };
+
+ReactDom.render(<App />, document.getElementById("root"));
--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-const App = () => {
-  return (
-    // html will be put inside return();
-    <>
-      {/* without this <> or <React.Fragment> or <div> there will be errors */}
-      <h1>Hello</h1>
-      <p>How are you?</p>
-    </>
-  );
-};
+import App from "./App";
 
 ReactDom.render(<App />, document.getElementById("root"));
